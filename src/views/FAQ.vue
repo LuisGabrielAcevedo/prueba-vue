cb70f06559ffaf62827aa1afeab637f703e75aff
Gabriel Acevedo <u631186@T0003540283.local>
2021-02-27 16:23:08 -0300

    faq

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -7,10 +7,137 @@
     <div class="faq__content-1">
       <div class="faq__content">
         <div class="faq__box">
-          <div class="faq__scroll">
-            <span class="faq__text">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-            </span>
+          <div class="faq__scroll faq__text">
+            <p class="faq__text-title">
+              1.	¿Para poder participar en la promoción Promogolazos que requisitos debo cumplir?
+            </p>
+            <p>
+              Para poder participar en la promoción debes ser mayor de edad (Cumplidos antes del 01 marzo del 2021), vivir en Colombia, al igual que, aceptar los términos y condiciones y tratamientos de datos de la actividad. 
+            </p>
+            <p class="faq__text-title">
+              2.	¿Cuándo inicia y cuando termina la promoción Promogolazos?
+            </p>
+            <p>
+              La Actividad estará vigente a partir del 01 de marzo y hasta el 30 de abril del 2021, o hasta agotar las existencias de los tazos incluidos en los productos participantes, lo que ocurra primero.
+            </p>
+            <p class="faq__text-title">
+              3.	¿Dónde encuentro los códigos para participar en la actividad Promogolazos?
+            </p>
+            <p>
+              Al interior de los empaques de producto “Papas MARGARITA” sabores: pollo, limón, BBQ, natural, flammin hot, tomate onduladas y mayonesa onduladas, en presentaciones de: 25g, 30g, 32g, 33g, 36g, 37g, 39g  identificados con la tirilla externa de la ACTIVIDAD, se encontrará un (1) Tazo® alusivo a uno de los 50 golazos históricos de la UEFA Champions Legue, el cual incluye un código alfanumérico único (en adelante el (los) Código(s))
+            </p>
+
+            <p class="faq__text-title">
+              4.	¿Cuántos caracteres tienen los códigos que se encuentran en los tazos?
+            </p>
+            <p>
+              El código es alfanumérico y tiene una extensión de 8 caracteres. Recuerda ingresarlo tal cual se encentra en el tazo.
+            </p>
+            <p class="faq__text-title">
+              5.	¿Con cuántos códigos se puede ganar el premio de recarga celular?
+            </p>
+            <p>
+              Cada 4 códigos que se registren de forma correcta. Hasta agotar existencias. Son 40.000 recargas celular.
+            </p>
+            <p class="faq__text-title">
+              6.	¿Cuántas recargas puede ganar un participante?
+            </p>
+            <p>
+              Cada participante podrá ganar máximo 4 recargas durante toda la ACTIVIDAD, es decir un máximo de $20.000.
+            </p>
+            <p class="faq__text-title">
+              7.	Si se acaban los premios de recargas de celular ¿puedo seguir concursando?
+            </p>
+            <p>
+              ¡Si! Son 40.000 recargas de celulares que hay disponibles, pero en el momento que se acaben puedes seguir participando por los premios de los bonos Falabella. Recuerda que entre más códigos registres más oportunidades tendrás de ganar. 
+            </p>
+            <p class="faq__text-title">
+              8.	¿De qué valor es el premio de recarga?
+            </p>
+            <p>
+              El valor de la recarga es de $5.000.
+            </p>
+            <p class="faq__text-title">
+              9.	Para recibir algún premio o participar en el concurso ¿debo consignar o transferir a alguna cuenta dinero o hacer algún pago en efectivo?
+            </p>
+            <p>
+              NO. Nunca se te pedirá ninguna consignación o pago para darte algún premio. Ten cuidado pues podrías ser víctima de estafa. Si tienes sospechas o dudas, no olvides escribirnos en la sección contáctenos. 
+            </p>
+
+            <p class="faq__text-title">
+              10.	¿Cuáles son los operadores con los que se puede ganar el premio de recarga? 
+            </p>
+            <p>
+              Las recargas aplican para los operadores CLARO, MOVISTAR, TIGO, AVANTEL, VIRGIN, ETB, ÉXITO, FLASH MOBILE
+            </p>
+            <p class="faq__text-title">
+              11.	¿Cómo recibo mi premio recarga?
+            </p>
+            <p>
+              Las recargas celulares se harán efectivas en el número celular del participante y el operador con el que se registró. La recarga podrá verse reflejada en un término de hasta 24 horas. No obstante, dicho termino podría presentar variaciones.
+            </p>
+
+            <p class="faq__text-title">
+              12.	Soy menor de edad. ¿Puedo participar?
+            </p>
+            <p>
+              No. Únicamente los mayores de edad pueden participar. Cumplidos antes del 1 de marzo de 2021. Es decir, al inicio de la promoción. 
+            </p>
+            <p class="faq__text-title">13.	Ingresé mal mi celular y/o mi operador ¿qué puedo hacer?</p>
+            <p>
+              En la sección de contáctenos puedes escribirnos indicando el número celular debemos actualizar al igual que el operador móvil al que pertenece la línea celular.
+            </p>
+            <p class="faq__text-title">14.	Tengo problema con mi recarga ¿qué puede ser?</p>
+            <p>Puedes escribirnos en la sección de contáctenos para revisar tu caso. Los problemas se pueden generar dependiendo del terminal, el tipo de cuenta del usuario y de los operadores, es posible que se presenten fallas en las tramitaciones de las recargas. En caso de que el usuario haya desconectado su terminal, en el caso de que el área geográfica en la que se encuentre el usuario no esté cubierta por los operadores telefónicos o en casos en que los usuarios de líneas celulares (participantes) adeuden a su operador, pertenezcan en las categorías de adelanta tu saldo, S.O.S o intermitencias en las recargas o tengan algún tipo de restricción para recibir la recarga.</p>
+            <p class="faq__text-title">
+              15.	¿Cómo participo por los bonos de Falabella?
+            </p>
+            <p>
+              Los diez (10) Participantes que para las 23:59:59 horas, del día treinta (30) de abril de 2021 cuenten como mayor número de códigos registrados, serán los ganadores cada uno de un premio mayor. Cada premio mayor corresponderá a un (1) bono por un valor equivalente a CUATRO MILLONES DE PESOS COLOMBIANOS ($4.000.000) que podrá ser redimido en todas las tiendas físicas Falabella a nivel nacional. Recuerda es muy IMPORTANTE que conserves los respectivos tazos para su correspondiente verificación. En caso de que el ORGANIZADOR solicite estos soportes y no sean presentados, el Participante -ganador podrá ser descalificado. 
+            </p>
+            <p class="faq__text-title">
+              16.	¿Dónde ver la posición que ocupo para el premio de las tarjetas Falabella?
+            </p>
+            <p>
+              Puedes consultar tu ubicación en la sección de RANKING. Esta sección te indicará la posición que ocupas y el acumulado parcial en el momento de la consulta con fecha y hora.
+            </p>
+
+            <p class="faq__text-title">
+              17.	Si existen varios participantes con la misma cantidad de códigos registrados. ¿cuál es el criterio para el desempate? 
+            </p>
+            <p>
+              En el evento en que resulte un empate entre dos o más participantes diferentes, el ganador será quien haya realizado los registros en menor tiempo. 
+            </p>
+            <p class="faq__text-title">18.	¿Cuántos bonos de tarjetas de Falabella son?</p>
+            <p>Son 10 tarjetas Falabella por valor de $4.000.000 cada uno.</p>
+
+            <p class="faq__text-title">19.	El código que está en mi tazo no funciona ¿Qué puedo hacer?</p>
+            <p>
+              Deberás escribirnos a través de la opción contáctenos para por validar tu caso.  Recuerda ingresar el código tal y como está en tu tazo y que el código se compone de números y letras con una extensión de 8 caracteres.
+            </p>
+
+            <p class="faq__text-title">
+              20.	Si al finalizar el concurso, estoy entre los 10 primeros puestos del ranking ¿qué debo hacer?
+            </p>
+
+            <p>
+              Los ganadores serán contactados por el ORGANIZADOR, dentro de los tres (03) días hábiles siguientes a la fecha en que fueron anunciados como ganadores por parte del ORGANIZADOR. Para este efecto, si los datos suministrados por los ganadores al ORGANIZADOR son erróneos y/o el ganador no responde a alguno de los dos (02) intentos de comunicación que el ORGANIZADOR realice al número celular (obligatorio) registrado por el ganador, se entenderá desistido el premio, en cuyo caso el ORGANIZADOR procederá a asignarlo de acuerdo con lo establecido en los términos y condiciones.
+            </p>
+
+            <p>
+              El Premio Mayor será entregado por el ORGANIZADOR a cada uno de los ganadores en la dirección física que estos informen al ORGANIZADOR, dentro de los veinte (20) días hábiles siguientes a la fecha en que los ganadores fueron contactados por el ORGANIZADOR, siempre que hayan aportado la documentación requerida por el ORGANIZADOR (copia del documento de identidad registrado, y suscrito el Acta de entrega correspondiente, así como los tazos con participo en la promoción.
+            </p>
+
+            <p class="faq__text-title">21.	¿A dónde puedo escribir en caso de dudas o preguntas?</p>
+            <p>
+              Todas las preguntas que tengas de la actividad debes realizarlas a través de la sección Contáctenos. Las respuestas a tus preguntas serán enviadas al correo electrónico que registraste. Las respuestas en algunas ocasiones podrán llegar al SPAM de tu correo a correos no deseados. O llamando al 01 8000 91 1053. 
+            </p>
+            <p class="faq__text-title">22.	¿Qué es FAQ?</p>
+
+            <p>Son preguntas frecuentes.</p>
+
+
+
           </div>
         </div>
       </div>
@@ -79,14 +206,27 @@ export default {
     @include mobile () {
       padding-right: 15px;
     }
-
+    p {
+      margin-bottom: 10px;
+    }
   }
   &__text{
-    font-family: MontrealBold;
-    font-size: 14px;
+    font-family: MontrealRegular;
+    text-align: justify;
+    font-size: 12px;
     @include mobile() {
       font-size: 10px;
     }
+  }
+  &__text-title {
+    font-weight: 600;
+    margin-left: 10px;
+  }
+  &__option {
+    margin-left: 10px;
+  }
+  &__option2 {
+    margin-left: 20px;
   }
     /* width */
   ::-webkit-scrollbar {
@@ -105,5 +245,4 @@ export default {
     border-radius: 10px !important;
   }
 }
-
 </style>
